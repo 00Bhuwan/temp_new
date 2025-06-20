1bd94c5e6bcbc3116c59b497ba346ff7956d1218
00Bhuwan <9843lalitpur@gmail.com>
2025-06-20 13:00:39 +0545

    file struct

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_DOT = '-e.'
+HYPEN_DOT = '-e .'
 
 
 def get_requirements(file_path: str) -> List[str]:
-    """"
-    This function reads the requirements file but removes the hypen_dot i.e. -e. if present
+    """
+    This functions reads the requirements file
+    but removes the -e . line if it exists.
     """
     requirements = []
     with open(file_path, 'r') as file:
-    requirements = file.readlines()
-    requirements = [req.repplace('\n', '') for req in requirements]
-    if HYPEN_DOT in requirements:
-    requirements.remove(HYPEN_DOT)
+        requirements = file.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if HYPEN_DOT in requirements:
+            requirements.remove(HYPEN_DOT)
     return requirements
 
 
 setup(
-    name='temp_now',
-    version=0.0.1,
-    author='00bhuwan',
+    name='pygpt4all',
+    version='0.1.0',
+    author='Bhuwan',
     author_email='joshibhuwan078@gmail.com',
-    description="A temperature prediction project",
+    description='A Python wrapper for the GPT-4 All API',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
